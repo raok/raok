d5862ca3ad42c4f6ee9ae62a99fa98e16269160e
Veronika Hillebrand <veronika.hillebrand@unboxedconsulting.com>
2014-03-14 15:41:07 +0000

    observing the acts now works

--- client/client.js
+++ client/client.js
@@ -1,20 +1,15 @@
 Acts = new Meteor.Collection('acts');
+var map;
 
-var actsSub = Meteor.subscribe("actsUpdated", updateActs);
-actsSub.ready(function() {
-    var acts = Acts.find();
-
-    console.log('hi');
-
-    acts.forEach(function (act) {
-      console.log(act);
-      var postMarker = L.marker([act.lat, act.lon]).addTo(map);
-      postMarker.bindPopup(act.description);
-    });
+Acts.find().observe({
+  added: function(act) {
+    postMarker = L.marker([act.lat, act.lon]).addTo(map);
+    postMarker.bindPopup(act.description);
+  }
 });
 
-function updateActs(coll) {
-  console.log(coll);
+function updateActs() {
+  console.log('hi');
 }
 
 Template.hello.greeting = function () {
@@ -22,34 +17,11 @@ Template.hello.greeting = function () {
 };
 
 Template.hello.rendered = function() {
-  var map = L.map('map').setView([51.505, -0.09], 13);
+  map = L.map('map').setView([51.505, -0.09], 8);
 
   L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
     maxZoom: 18
   }).addTo(map);
-
-  setTimeout(function() {
-    var acts = Acts.find();
-
-    acts.forEach(function (act) {
-      var postMarker = L.marker([act.lat, act.lon]).addTo(map);
-      postMarker.bindPopup(act.description);
-    });
-
-  }, 200);
-
-
-  // this is just test
-  var marker = L.marker(['51.5', '-0.09']).addTo(map);
-  marker.bindPopup('Help Me?');
 };
-
-Template.hello.events({
-  'click input': function () {
-    // template data, if any, is available in 'this'
-    if (typeof console !== 'undefined')
-      console.log("You pressed the button");
-  }
-});
 
