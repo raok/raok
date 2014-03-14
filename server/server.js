aa164c06702f3653736dbcb57c4f0d0609afbdab
nelsonic <nodecoder@gmail.com>
2014-03-14 13:50:31 +0000

    app working with twitter api

---
+++ server/server.js
@@ -0,0 +1,13 @@
+var twitter = Meteor.require('twitter'),
+	util = Meteor.require('util'),
+	twit = new twitter({
+    consumer_key: 'wyir0dDuntZkbXF0jQps8w',
+    consumer_secret: 'hrWA0pEoGGD9DiJf1tanhkgYOCNwFL7yN6L8QCc3Nc',
+    access_token_key: '2389016353-maPa5ax7R3VcXnFBROMb8HPEwJsO64So62dAHnK',
+    access_token_secret: 'iKuvsT3tZd0Mk8ACUCfi6KzeN3Fvbr5EnyzDyHIlUgrrA'
+});
+
+twit.search('help', function(data) {
+    console.log(util.inspect(data));
+    
+});
