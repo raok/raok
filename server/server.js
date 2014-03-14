c9559c1312a39eef8da4336c10ed0bab2eb71424
nelsonic <nodecoder@gmail.com>
2014-03-14 15:40:28 +0000

    tweets get inserted into mongo! #GreatSuccess

--- server/server.js
+++ server/server.js
@@ -1,9 +1,7 @@
-Acts = new Meteor.Collection("acts");
-
-
+// Acts = new Meteor.Collection("acts");
 var twitter = Meteor.require('twitter'),
 	util = Meteor.require('util'),
-	fs   = Npm.require('fs'),
+	// Fiber = Meteor.require('fibers'),
 	twit = new twitter({
     consumer_key: 'wyir0dDuntZkbXF0jQps8w',
     consumer_secret: 'hrWA0pEoGGD9DiJf1tanhkgYOCNwFL7yN6L8QCc3Nc',
@@ -11,46 +9,43 @@ var twitter = Meteor.require('twitter'),
     access_token_secret: 'iKuvsT3tZd0Mk8ACUCfi6KzeN3Fvbr5EnyzDyHIlUgrrA'
 });
 
-twit.search('help', function(data) {
-    // console.log(util.inspect(data));
-    // data.forEach()
-    // Acts.forEach()
-    console.log('\n - - - - - - - - - - - - - - -  \n');
-    console.log(data);
-    t = data.statuses;
-    for (var k in t){
-    	if (t.hasOwnProperty(k)) {
-         console.log("Key is " + k + ", value is");
-         console.log(t[k].text)
-         // Acts.insert({'text':t[k].text});
-    	}
-		}
-    // fs.writeFile(process.env.PWD+'/data.txt', tweets.toString(), function(err, data){
-    // 	if(err) console.log(err)
-    // 	console.log(data);
-    // })
-	
-// fs.writeFile(process.env.PWD+'/helloworld.txt', 'Hello World!', function (err) {
-//   if (err) return console.log(err);
-//   console.log('Hello World > helloworld.txt');
-// });
-   //  var tweets = JSON.parse(data);
-  	// for(var i = 0; i < tweets.length; i++){
-  	// 	console.log("- - - - - - - - - - - - - - NEXT ");
-  	// 	console.log(tweets[i]);
-  	// }
+Meteor.startup(function () {
 
-  	// if(typeof data.statuses !== "undefined"){
-	  // 	data.statuses.forEach(function(tweet){
-	  // 		if (typeof tweet.text !== 'undefined'){
-		 //  		console.log("- - - - - - - - - - - - - - NEXT ");
-		 //  		console.log(tweet.text);
-		 //  		var json = JSON.pase(tweet);
-		 //  		Acts.insert(json);
-	  // 		}
-	  // 	})	
-  	// }
+  var insertTweet = Meteor.bindEnvironment(function(tweet) {
+    Posts.insert({'text':tweet});
+  });
+		// console.log(Fiber);
 
+	Posts = new Meteor.Collection("posts");
+// Posts.insert({title: "Hello world", body: "First post"});
 
+	function getTweets(callback){
+		// Fiber(function() { 
+				twit.search('help', function(data) {
 
+			    // Acts.forEach()
+			    if (data.geo){
+		        acts.emit('update', {coords: data.geo.coordinates, lang: data.lang, msg:data.text});
+		      }
+
+			    console.log('\n - - - - - - - - - - - - - - -  \n');
+			    console.log(data);
+			    t = data.statuses;
+			    for (var k in t){
+			    	if (t.hasOwnProperty(k)) {
+			         // console.log("Key is " + k + ", value is");
+			         console.log("- - - - - - - - - - - - - -  ");
+			         console.log(t[k].text)
+			         // Posts.insert({title: t[k].text});
+			         insertTweet(t[k].text);
+			         // Acts.insert({'text':t[k].text.toString()});
+			    	}
+					}
+				callback();
+			}); // end twit.search
+		// }).run();  
+	}
+	getTweets(function(){
+		console.log('this');
+	});
 });
