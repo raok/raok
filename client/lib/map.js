59e6726b6d7901278f5cc1067192bf19dbbb19d0
Veronika Hillebrand <veronika.hillebrand@unboxedconsulting.com>
2014-03-16 12:44:46 +0000

    Moving all the map stuff into its own client library

---
+++ client/lib/map.js
@@ -0,0 +1,39 @@
+Map = function() {
+
+  var map = {};
+
+  function getMapBounds() {
+    var point = map.$el.getBounds(),
+        bounds = {
+          maxLat: point.getNorth(),
+          minLong: point.getWest(),
+          minLat: point.getSouth(),
+          maxLong: point.getEast()
+      };
+    return bounds;
+  };
+
+  map.init = function() {
+    map.$el = L.map('map').setView([0, 0], 1, { animate: true});
+    map.$el.locate({setView: true, maxZoom: 9});
+    Session.set('bounds', getMapBounds());
+
+    map.$el.on('moveend', function() {
+      map.$el.stopLocate();
+      Session.set('bounds', getMapBounds());
+    });
+
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+      maxZoom: 18
+    }).addTo(map.$el);
+  };
+
+  map.addPin = function(act) {
+    var postMarker = L.marker([act.lat, act.lon]).addTo(map.$el);
+    postMarker.bindPopup('<p>' + act.description + '</p><p><a href="https://twitter.com/intent/tweet?in_reply_to=' + act.id + '&hashtags=offertohelp">Offer to help</a></p><p><a href="https://twitter.com/intent/tweet?in_reply_to=' + act.id + '&hashtags=done">I did this</a></p>');
+  };
+
+  return map;
+
+};
--- client/main.js
+++ client/main.js
@@ -2,43 +2,17 @@ Deps.autorun(function() {
   Meteor.subscribe("actsUpdated", Session.get("bounds"));
 });
 
-var map;
+var map = Map();
 
 Acts.find().observe({
-  added: function(act) {
-    postMarker = L.marker([act.lat, act.lon]).addTo(map);
-    postMarker.bindPopup('<p>' + act.description + '</p><p><a href="https://twitter.com/intent/tweet?in_reply_to=' + act.id + '&hashtags=offertohelp">Offer to help</a></p><p><a href="https://twitter.com/intent/tweet?in_reply_to=' + act.id + '&hashtags=done">I did this</a></p>');
-  }
+  added: map.addPin
 });
 
 Template.hello.greeting = function () {
   return "Welcome to raok.";
 };
 
-function getMapBounds() {
-  var point = map.getBounds(),
-      bounds = {
-        maxLat: point.getNorth(),
-        minLong: point.getWest(),
-        minLat: point.getSouth(),
-        maxLong: point.getEast()
-    };
-  return bounds;
-};
-
 Template.map.rendered = function() {
-  map = L.map('map').setView([0, 0], 1, { animate: true});
-  map.locate({setView: true, maxZoom: 9});
-  Session.set('bounds', getMapBounds());
-
-  map.on('moveend', function() {
-    map.stopLocate();
-    Session.set('bounds', getMapBounds());
-  });
-
-  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-    maxZoom: 18
-  }).addTo(map);
+  map.init();
 };
 
